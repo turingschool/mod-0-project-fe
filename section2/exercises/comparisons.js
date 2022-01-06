8c14a46ebadcea234bf7acc2d3202b102bc3d0f9
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-01-06 14:34:46 -0600

    Add Section 2 work

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -24,9 +24,10 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
+console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
@@ -36,7 +37,7 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
-
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
 since the numberTeachers is an integer value and the stringTeachers is a string value, although they are both equal to 4,
@@ -49,18 +50,20 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
-
+console.log( "Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 
 //-------------------
@@ -74,21 +77,25 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
+// the console log is going to evaluate the result of the comparison: is 4 less than 9? The result should come up true because 4 is less than 9
 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+//the console log is going to evaluate the result of the comparison: is books greater than 4? The result should come up false because 4 is not less than 3
+
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+// the console log is going to evaluate the result of the comparison: is friends greater than siblings? The result should come up true because 6 is greater than 2
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+// the console log is going to evaluate the result of the comparison: is attendees not equal to meals? The result should come up true because 8 is not equal to 9
 
 //-------------------
 // PART 3: Logical Operators
@@ -110,18 +117,22 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log(lovesToPlay && lovesTreats);
 
 
 // Determine if the dog loves to play and loves the dog park
+console.log(lovesToPlay && lovesDogPark);
 
 
 // Determine if the dog loves to play or loves the dog park
+console.log(lovesToPlay || lovesDogPark);
 
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && isAPuppy);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: It came us a a refernece error because isAPuppy is not defined anywhere so it cant determine if this condition is true of false.
 
 //-------------------
 // FINAL CHECK
--- section2/reflection.md
+++ section2/reflection.md
@@ -1,15 +1,33 @@
 ## Section 2 Reflection
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
+* Setting S.M.A.R.T goals can help learning code with focus, clarity, and use of appropriate strategies.
+* Understanding that I will make mistakes, and thats not a bad thing. I need to use my mistakes as a critical part of learning.
 
 2. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+### I currently demonstrate a Growth Mindset in the following ways:
+1. I believe I can improve my coding skills with effort and practice.
+2. I am pretty good at accepting feedback from others.
+
+### I currently don't demonstrate a Growth Mindset in the following ways:
+1. I need to be better at seeing my mistakes as a part of learning.
 
 3. In this section, we removed some of the supports that you had in Section 1. We didn't give the directions for how to run a file in node and made some directions a bit more vague, intentionally. How did that feel? What was uncomfortable about it? How did it support your learning?
+* It was shocking to see, but I completely understand why it was done. It was uncomfortable because I had to stop what i was working on and go back to the old files and actually learn how what I was copying and pasting worked. Im happy that I now know what to type out to run a file in a node instead of relying on copying and pasting the correct way.
 
 4. What is a conditional statement? Give one example of a daily life conditional. Give one example of where a conditional is probably used in a web application you use.
+1. A conditional statement is a line of code that instructs the computer on what decision to make when given a set of conditions.
+2. If there are dishes in the sink, I need to wash them. If there aren't dishes in the sink, I don't have to wash the dishes.
+3. If the password and username are correct, then you are successfully logged in. If the password or username aren't correct, you will be prompted to try again.
 
 5. How do you add multiple conditions to an `if` statement? In your own words, explain how to program reads them and determines what to do.
+* You add multiple conditions by using a logical operator such as "&&"
+* "&&" is read, by the computer as "and." so the computer would see the first condition and know that that condition needs to be met as well as the following condition because it read the "&&".
+* The computer would read this line of code `if ((cupsOfFlour <= 1) && (hasSauce = false)) {
+  console.log("I cannot make pizza");`as: if the variable cupsOfFlour is less than or equal to 1 AND the variable hasSauce is equal to FALSE, then console log the string "I cannot make pizza"
 
 6. What tools are available to you, as a developer, if you want to check that TWO conditions are met? Or, if you want to check that 1 of 2 conditions are met, before running a block of code?
+* You have logical operators to check if two conditions are met by using "&&" and you have logical operators to check if one of two conditions are met by using "||"
 
 7. What questions do you still have about `if` statements and/or functions?
+* Can you use "&&" and "||" in the same condition?
