129236ae8d39875e6ff2249d53d00e587e2c8faf
Shane Warning <shane.k.warning@gmail.com>
2022-01-09 13:18:51 -0600

    Initial commit

--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -22,7 +22,9 @@ var creatures = "unicorns";
 console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here: Line 22 will be logged as string. This is because
+// of "type coercion". Javascript conversts the number into string so the two data dataTypes
+// can be combinded together.
 
 
 // YOU DO:
@@ -31,6 +33,7 @@ console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
+console.log(`The ${speedy} jumped over the ${slowPoke}`)
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -42,14 +45,14 @@ speedy = "hare";
 
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation. 
+  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
