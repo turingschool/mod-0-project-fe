143ff118255aec26469ae9f7381a94845cd8e067
Grant Beard <Grantxbeard@gmail.com>
2022-01-10 11:44:51 -0700

    Add section 1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,7 +22,8 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
+// "My zoo has 7 unicorns!" because numberOfCreatures = 7 and creatures = unicorns.
 
 
 // YOU DO:
@@ -30,7 +31,7 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
-
+console.log("The" + speedy + "jumped over the" + slowPoke +)
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -39,17 +40,21 @@ var slowPoke = "lazy brown dog";
 
 slowPoke = "tortoise";
 speedy = "hare";
+console.log("In a predictable result, the" + slowPoke + "beat the" + speedy +"!")
 
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
-
+var me = "Grant";
+var food = "Pizzas";
+var quantity = 3;
+console.log(+ me + "has" + quantity + "whole" + food +"!")
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -23,20 +23,22 @@ console.log(enrolledStudents);
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
+var name = "Harry Potter";
+console.log(+ name + "must not return to Hogwarts")
 
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
+// How did you decide to use the data type you did for each of the three variables above?
   // Explain.
 
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
 
@@ -51,5 +53,5 @@ var junkFoodSnacks = 8;
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
