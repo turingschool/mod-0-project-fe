a0184b869136969a44dbf30c01d695a1bafb918b
Patrick Becker <patrickgbecker@gmail.com>
2022-01-12 15:20:13 -0600

    Add Section 3

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,17 +24,21 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
 
-
+let newLength = animals.push('Gorilla')
+console.log(animals);
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
 
+animals.splice(2, 0, "Beaver");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+let third = animals[3]
+console.log(third);
 
 //-------------------
 // PART 2: Foods: Array Methods
@@ -42,25 +46,29 @@ console.log(animals[0]);
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
 
-
+let foods = ['Brie Cheese','Prosciutto','Croissant','Olives']
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+let addFood = foods.push('Broccolini')
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+let byeFood = foods.pop()
+console.log(foods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+foods.splice(4, 0, "Hummus","Red Peppers","Cucumbers")
+console.log(foods);
 // YOU DO: Remove the food that is in index position 0.
-
+const firstFood = foods.shift();
+console.log(foods);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +87,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Spotify uses arrays for each artists songs, albums, playlists, etc.
+// 2: Reddit probably creates arrays for each posts comment section.
+// 3: GrubHub probably uses arrays for the adjustable number of menu items you can order.
 
 
 //-------------------
@@ -96,18 +104,31 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
 */
-var percentBatteryLeft = 12;
-var hasCharger = true;
-var chargerType = "car";
+var percentBatteryLeft = 14;
+var hasCharger = false;
+var chargerType = "phone";
 
-// Write your conditional here
-
-
+// Conditional statement 'If' will run the log command if the variables value meets the conditional operators evaluation.
+if (percentBatteryLeft >= 15) {
+  console.log("Congrats! You can call an Uber!");
+// Second command will execute if conditional operators in the second brackets evaluate to true.
+} else if (percentBatteryLeft <= 14, hasCharger === true && chargerType === "car") {
+  console.log("Luckily, You can call an Uber!");
+//Third command will execute if the first two conditions are not met and the statements in third bracket are met.
+} else if (percentBatteryLeft <= 14, hasCharger === false && chargerType === "car") {
+  console.log("I'm sorry, You cannot call an Uber.");
+//Otherwise, if none of the previous conditions are met, the last statement will execute.
+} else if (percentBatteryLeft <= 14, hasCharger === true && chargerType !== "car") {
+  console.log("I'm sorry, You cannot call an Uber.");
+//This statement will run if none of the variables meet any of the operators evaluations. 
+} else if (percentBatteryLeft <= 14, hasCharger === false && chargerType !== "car") {
+  console.log("I'm sorry, good luck finding a ride.");
+}
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
 // what the line of code below, does.
--- section3/exercises/loops-arrays.js
+++ section3/exercises/loops-arrays.js
@@ -10,7 +10,7 @@ Make sure to run the file with node in your command line.
 
 // EXAMPLE: Write code below that iterates through a list of animals and print
 // each animal.
-var animals = ["Zebra", "Giraffe", "Elephant"];
+var animals = ["Danger Noodle", "Cobra-Chicken", "Trash Panda"];
 
 for (var i = 0; i < animals.length; i++) {
   console.log(animals[i]);
@@ -19,21 +19,31 @@ for (var i = 0; i < animals.length; i++) {
 // YOU DO: Write code below that iterates through a list of animals and prints "The
 // <animal> is awesome!" for each animal.
 
-
+for (var i = 0; i < animals.length; i++){
+  console.log("The " + animals[i] + " is awesome!");
+}
 
 // YOU DO: Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
 
-
+let foods = ['gummy bears', 'wasabi peas', 'chocolate peanuts', 'jalapeno chips', 'helados', 'pizza rolls']
+for (let i = 0; i < foods.length; i++){
+  console.log("Add " + foods[i] + " to shopping list.");
+}
 
 // YOU DO: Create an array of numbers and then iterate over that array to log double
 // of each of the number. (Ex: 4 doubled is 8.)
-
+var num = [1, 2, 3, 4, 5];
+for (i = 0; i < num.length; i++) {
+  console.log(num[i] * 2);
+}
 
 // YOU DO: Using the array of names below, print out "Happy Birthday <name>" for
 // each name except the first two.
 var names = ["Sevy", "Lindsey", "Jericho", "Raeonna", "Latrina", "Jhana", "Luca"];
-
+for (i = 2; i < names.length; i++){
+  console.log("Happy Birthday " + names[i]);
+}
 
 
 // THE FINAL YOU DO: Using the array of ages below, print out a statement for each age.
@@ -42,8 +52,16 @@ var names = ["Sevy", "Lindsey", "Jericho", "Raeonna", "Latrina", "Jhana", "Luca"
   // 18 or over? "Make sure you get out and vote!"
 
 // WORD FROM THE WISE: Before you start coding, jot down, with pen and paper, what steps you
-// want to tell the program to take. Think of all the concepts you've learned so far. 
+// want to tell the program to take. Think of all the concepts you've learned so far.
 
 // Remember, stuck for 15-20 minutes? Reach out to your Mod 0 cohort on Slack.
 
-var ages = [17, 20, 34, 18, 16, 65, 49, 10, 22, 18];
+var ages = [17, 20, 34, 18, 16, 65, 49, 10, 22, 18]
+
+for (i = 0; i < 10; i++) {
+  if (ages[i] >= 18) {
+     console.log("Make sure you get out and vote!");
+  } else if (ages[i] <= 17) {
+    console.log("Oh no, you can't vote quite yet.");
+  }
+}
