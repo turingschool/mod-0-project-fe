b658547b749e3839c9d6f63c94f1d26d4873ae5f
Michael Martinelli <turtelboy123@gmail.com>
2022-01-13 11:47:29 -0700

    Add Exercise 3

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -25,42 +25,50 @@ console.log(animals[0]);
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
 
-
+console.log(animals.length);
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals.push("Gorilla");
+console.log(animals);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
 
-
+animals[2] = "wolf";
+console.log(animals);
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.unshift("Elephant");
+console.log(animals);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ["Steak" , "Salad" , "Chicken" , "Pork"];
+console.log(foods);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push("broccoli");
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop();
+console.log(foods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+foods.unshift("Celery" , "Cashews" , "Salami");
+console.log(foods);
 // YOU DO: Remove the food that is in index position 0.
-
+foods.shift();
+console.log(foods);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +87,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Facebook friends list
+// 2: Lobbys in video games(player names)
+// 3: Youtube playlists (name of songs)
 
 
 //-------------------
@@ -96,18 +104,23 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
 */
-var percentBatteryLeft = 12;
+var percentBatteryLeft = 15;
 var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
 
+if (percentBatteryLeft >= 15) {console.log('I can uber') //Starts of by checking if the battery is at or above 15 to uber
+}  else if (hasCharger === true && chargerType === "car")//Afterwards, if the previous requirement wasn't met, it checks if you have a charger and its for a car, if it is, then you can still uber.
+    {console.log("I can uber")
+} else { console.log("I cannot uber") //if neither requirement are met, then you cannot uber.
 
+};
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
 // what the line of code below, does.
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,13 +1,14 @@
 ## Section 3 Reflection
 
 1. In Section 3, we removed even more supports and pushed you to learn a bit more independently, read through technical documentation, and use your resources more. How did that feel?
-
+It was refreshing, but somewhat challenging. It was challenging in the way I didn't have something to tell me what exactly to code, like it was in section 1, but it was also nice because when I found the answer, I felt like I knew what I was doing and was much more confident about coding. The only slip ups I had this time around was remembering how to structure if statements.
 2. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
-
+Arrays are probably used in things like youtube queue lists, facebook friends lists, anything that has a group of a data type in it like integers or strings.
 3. In your own words, explain what an Array method is.
-
+Array methods are like new functions that we can add to our arrays to create a different output when we log our results.
 4. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
-
+A loop helps us go through our array and put out the index values. We would use a loop with our array if we needed to grab a certain element from our array. We would not use arrays if we had to find the value of something generic or print out multiple copies of something, instead of unique values.
 5. If you had to teach a 5 year old about loops, how would you explain it?
-
+We use these loops to repeat code a certain number of times.Starting off, you create a starting point.Afterwards, you create how many times the code will loop. Then, you make sure that after the code has gone through an iteration, if the number of times looped isn't met, it continues to do so. Once you have looped through the code your set amount of times, you print out the results.
 6. What questions do you still have about Arrays and loops?
+Is there a better way to loop through this kind of stuff? I noticed that sometimes you can really mess up code or expected outcomes if you type out the wrong value or key. Will we learn about those things if there are?
