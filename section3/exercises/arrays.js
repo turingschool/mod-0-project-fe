9cbd5b04bd461eec123bda77ea4948052ae4e81c
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-01-14 00:42:05 -0700

    Complete Part A

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,53 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals.pop();
+animals.push("Gorilla");
+console.log(animals);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Lion");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.unshift("Elephant");
+console.log(animals[0]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var favoriteFoods = ["Quinoa" , "Kale" , "Avocado" , "Ice Cream"];
+console.log(favoriteFoods);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(favoriteFoods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+favoriteFoods.unshift("Broccoli");
+console.log(favoriteFoods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+favoriteFoods.pop();
+console.log(favoriteFoods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+favoriteFoods.push("Brussels Sprouts" , "Sushi" , "Pizza");
+console.log(favoriteFoods);
 
 // YOU DO: Remove the food that is in index position 0.
-
+favoriteFoods.shift();
+console.log(favoriteFoods);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,13 +89,16 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
-
+// 1: Baggu- Shopping Cart
+// var shoppingCart = ["baby baggu in raspberry happy" , "fabric mask set in cow print" , "duck bag in cedar"];
+// In reality these strings would probably be the item number but for the sake of demonstration.
+// 2: Hulu- Watchlist
+// var watchList = ["Ugly Betty" , "King of The Hill" , "Broad City"];
+// 3: Gmail- Inbox/ Unread
+// var unreadEmails = ["Telfar" , "Notion" , "ClassPass"];
 
 //-------------------
 // Part 4: Don't let yourself forget everything from Section 2 of the project
@@ -96,7 +109,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,7 +119,15 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+// Here, I am establishing that if the percentage of battery left is greater than or equal to 15%, a script will run that notifies the user they have successfully called their Uber.
+if (percentBatteryLeft >= 15) {
+  console.log("Your Uber will arrive shortly.");
+  // In this line I am addressing what script to run if the percentage of battery life is less than or equal to 15. In this case, it is notifying the user that they cannot call a ride until their device has a battery percentage of 15% or more.
+} else if (percentBatteryLeft <= 15) {
+  console.log("Unable to call ride. Please try again once battery percentage is 15% or higher.");
+} 
 
+// Since the second sentence of the prompt mentions that it doesn't matter if the user has a charger at all or the type of charger they have, I decided to leave the conditional here as it runs as desired given different percentBatteryLeft inputs.
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
