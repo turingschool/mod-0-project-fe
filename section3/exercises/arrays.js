5727a6dfef9e5a20456a99519b70d2d16c516eac
Neil Linton <lintonbneil@gmail.com>
2022-01-14 07:27:46 -0800

    Added section 3

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,57 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
+animals[2] = "Gorilla";
+
+console.log(animals);
 
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
+animals.push("Lion");
 
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
+animals.push("Elephant");
 
+console.log(animals);
+console.log(animals[4]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
+var food = ["Chips", "Salsa", "Oranges", "Apples"];
 
+console.log(food);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(food.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+food.push("broccoli");
+console.log(food);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+console.log(food.pop());
+console.log(food);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+food.push("Watermelon", "Strawberries", "Carrots");
+console.log(food);
 // YOU DO: Remove the food that is in index position 0.
-
+console.log(food.shift());
+console.log(food);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +93,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Facebook - set of posts, albums, and newsfeed.
+// 2: Spotify - list of songs dedicated to account.
+// 3: Netflix - list of movies dedicated to specific account.
 
 
 //-------------------
@@ -96,7 +110,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,7 +120,21 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
-
+/* This is an if conditional using a logical operator,
+ where as if percentBatteryLeft is greater than or equal too 15 and (&&)
+ hasCharger is strictly equal to true.
+ */
+if ((percentBatteryLeft >= 15) && (hasCharger === true)) {
+/* If the if conditional is true, the console will log the results in the terminal with the string
+"User can call for uber"
+*/
+  console.log("User can call for uber");
+// else where if the varaibles do not meet the conditions, they will prompt different results in a different string.
+} else {
+// else statement if the conditions do not mean true, than the string will prompt
+// "User cannot call for uber"
+  console.log("User cannot call for uber");
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
