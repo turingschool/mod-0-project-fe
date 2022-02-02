442f9ec0334dde09272951bcee6d872856130e0e
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-02-02 12:23:11 -0800

    Add Section 1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -19,37 +19,41 @@ console.log("The " + team + " are " + name + "'s favorite Quidditch team");
 var numberOfCreatures = 7;
 var creatures = "unicorns";
 
-console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
+console.log("My zoo has " + numberOfCreatures + " " + creatures + "!");
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
-
+// Your answer and explanation here:
+//A string, JS will covert the number into a string, the rest are already strings.
 
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "The quick red fox jumped over the lazy brown dog":
-var speedy = "quick red fox";
-var slowPoke = "lazy brown dog";
-
+var speedy = "quick red fox ";
+var slowPoke = "lazy brown dog ";
+console.log("The " + speedy + "jumped over the " + slowPoke);
 
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
 
-slowPoke = "tortoise";
-speedy = "hare";
+slowPoke = "tortoise ";
+speedy = "hare!";
+console.log("In a predictable result, the " + slowPoke + "beat the " + speedy);
 
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
-
+var favFood = "pepperoni pizza, ";
+var favDrink = "Mountain Dew CodeRed, ";
+var favSnack = "flaming hot cheetos.";
+console.log("I fell asleep after having " + favFood + favDrink + "and " + favSnack);
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -23,33 +23,42 @@ console.log(enrolledStudents);
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
-
+var dobby = 'Harry Potter must not return to Hogwarts';
+console.log(dobby);
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
-
+var firstName = 'Masaki';
+var isHungry = true;
+var numberOfPets = 0;
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
+// How did you decide to use the data type you did for each of the three variables above?
   // Explain.
-
+//The first one was a name so I used a string since it is text.
+//The Second one I was not quite as sure on. But I think it's a Boolean since it is a yes or no.
+//The third one was a number, because number of pets.
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
-
-
+firstName = 'Michael';
+isHungry = false;
+numberOfPets = 2;
+console.log(firstName)
+console.log(isHungry);
+console.log(numberOfPets);
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
-
+console.log(healthySnacks + junkFoodSnacks)
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
