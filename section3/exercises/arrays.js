2beaffd5b0b6799ea0d84fbb5eed4b466dbf8ee8
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-02-10 10:21:22 -0700

    Updated all exercises

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,56 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+var numAnimals ;
+numAnimals = animals.length;
+console.log(numAnimals);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals.splice(2, 1, "Gorilla");
+console.log(animals);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Lions");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.unshift("Elephants");
+console.log(animals);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var italianFoods = ['spaghetti','lasagna','pizza','calzones'];
+console.log(italianFoods);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+var numItalianFoods ;
+numItalianFoods = italianFoods.length
+console.log(numItalianFoods)
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+italianFoods.push("broccoli")
+console.log(italianFoods)
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+italianFoods.pop()
+console.log(italianFoods)
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+italianFoods.push("spumoni","carbonara","penne")
+console.log(italianFoods)
 
 // YOU DO: Remove the food that is in index position 0.
-
+italianFoods.shift()
+console.log(italianFoods)
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +92,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: AppleMusic: stores lists in the form of playlists, where it remembers groups of songs that I put together.
+// 2: AppleMusic: stores arrays in the form of albums, where songs are grouped together inside of that album.
+// 3: AppleMusic: stores different album names from the same artist on their 'albums' page as part of an array.
 
 
 //-------------------
@@ -96,7 +109,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,6 +119,12 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+if (hasCharger && // 'if' (calling conditional) user hasCharger is 'true', AND ALSO if
+  chargerType === "car") { // charger type absolutely is a "car"
+  console.log('Can call an Uber!'); // the string to run will be this
+} else {
+  console.log('You cannot call an Uber, sorry!') // if both conditions are not met, this is the output.
+}
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
--- section3/exercises/loops-arrays.js
+++ section3/exercises/loops-arrays.js
@@ -18,22 +18,35 @@ for (var i = 0; i < animals.length; i++) {
 
 // YOU DO: Write code below that iterates through a list of animals and prints "The
 // <animal> is awesome!" for each animal.
-
+for (var i = 0; i < animals.length; i++) {
+  console.log(animals[i] + " is awesome!");
+}
 
 
 // YOU DO: Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
+var foodNames = ["doughnuts","apples","milk","bread"];
 
+for (var i = 0; i < foodNames.length; i++) {
+  console.log("Add " + foodNames[i] + " to shopping list");
+}
 
 
 // YOU DO: Create an array of numbers and then iterate over that array to log double
 // of each of the number. (Ex: 4 doubled is 8.)
+var nums = [7, 10, 19, 30];
 
+for (var i = 0; i < nums.length; i++) {
+  console.log(nums[i] * 2);
+}
 
 // YOU DO: Using the array of names below, print out "Happy Birthday <name>" for
 // each name except the first two.
 var names = ["Sevy", "Lindsey", "Jericho", "Raeonna", "Latrina", "Jhana", "Luca"];
 
+for (var i = 2; i < names.length; i++) {
+  console.log("Happy Birthday " + names[i]);
+}
 
 
 // THE FINAL YOU DO: Using the array of ages below, print out a statement for each age.
@@ -42,8 +55,14 @@ var names = ["Sevy", "Lindsey", "Jericho", "Raeonna", "Latrina", "Jhana", "Luca"
   // 18 or over? "Make sure you get out and vote!"
 
 // WORD FROM THE WISE: Before you start coding, jot down, with pen and paper, what steps you
-// want to tell the program to take. Think of all the concepts you've learned so far. 
+// want to tell the program to take. Think of all the concepts you've learned so far.
 
 // Remember, stuck for 15-20 minutes? Reach out to your Mod 0 cohort on Slack.
 
 var ages = [17, 20, 34, 18, 16, 65, 49, 10, 22, 18];
+console.log(ages);
+
+for (var i = 0; i < ages.length; i++) {
+  if (ages[i] < 18) console.log(ages[i] + "? Oh no, you cant vote yet.");
+ else console.log(ages[i] + "? Make sure you get out and vote!")
+}
