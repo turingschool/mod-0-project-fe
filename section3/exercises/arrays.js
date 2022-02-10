273ffba253ee9e01a8dd68dde3058e6dae4c8ca7
Cyanne Jones <jonescyanne@yahoo.com>
2022-02-10 12:32:26 -0700

    Add section3

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,48 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = "Gorilla";
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Dragon");
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.push("Elephant");
+console.log(animals[4]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foodArray;
+foodArray = ["Burritos", "French Fries", "Tacos", "Strawberries"];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foodArray.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foodArray.unshift("Broccoli");
+console.log(foodArray);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foodArray.pop();
+console.log(foodArray);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+foodArray.push("Ice cream", "Pizza", "Sushi");
+console.log(foodArray);
 // YOU DO: Remove the food that is in index position 0.
-
+foodArray.shift();
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +84,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: In Wikipedia, the list of films an actor has been in (their filmography) might be stored in an array.
+// 2: On Twitch, the pages you're subscribed to might be stored in an array.
+// 3: On Youtube, the videos you've "save(d) for later" might be stored in an array.
 
 
 //-------------------
@@ -96,7 +101,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -107,6 +112,20 @@ var chargerType = "car";
 
 // Write your conditional here
 
+/* This line of code is setting up our conditional.
+The first part is asking if your battery has 15% or more of a charge.
+if this conditional exaluates false (you have less than 15% battery remaining),
+it evaluates if you have a charger, and your charger is a car type.
+*/
+if ( (percentBatteryLeft >= 15) || ((hasCharger) && (chargerType === "car")) ) {
+//This line is logging the string "You can call an Uber!" to the console if line 120 evaluated true.
+  console.log("You can call an Uber!");
+//This line is setting up what happens if our if statement from line 120 evaluates false.
+} else {
+//This line is logging the string "You cannot call an Uber😰" to the console if line 120 evaluated false.
+  console.log("You cannot call an Uber 😰");
+//This line is closing out our if...else statement with closing curly braces and a semi-colon.
+};
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,12 +2,44 @@
 
 1. In Section 3, we removed even more supports and pushed you to learn a bit more independently, read through technical documentation, and use your resources more. How did that feel?
 
+It was challenging, but I think I really enjoyed it. I like how learning more independently feels a little like exploration and discovery and is ultimately very rewarding.
+
 2. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
+
+Using Slack as an example, all of the channels a user follows are probably stored in an array.
 
 3. In your own words, explain what an Array method is.
 
+An array method is a tool you can use to manipulate your array. Using the syntax of arrayName.methodName() you can add things to your array, remove things, get the length, probably anything you can think of yo make your array do very specific things.
+
 4. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
+
+A developer can use a loop to perform the same task a specific number of times. One can use a loop without an array when evaluating just one variable like logging the same string to the console 5 times like:
+
+> ```javascript
+> for (var i = 0; i < 5; i++) {
+> console.log("This is my string!");
+> };
+
+They can also be paired with arrays to evaluate the condition paired with each element in the array. This can be used for something like evaluating if an array of plants is a fruit or a vegetable and logging the result.
+> ```javascript
+> var plants = ["Broccoli", "Apple", "Orange", "Onion", "Tomato"];
+> for (var i = 0; i < plants.length; i++) {
+> if ((plants[i] === "Broccoli") || (plants[i] === "Onion")) {
+> console.log(`You are a ${plants[i]}! You are a vegetable!`);
+> } else if ((plants[i] === "Apple") || (plants[i] === "Orange")) {
+> console.log(`You are a ${plants[i]}! You are a fruit!`);
+> } else {
+> console.log(`You are a ${plants[i]}! You are an unknown plant!`);
+> };
+> };
+
+Just a side note, I added tomato to the unknown category, because does anyone really know what it is?
 
 5. If you had to teach a 5 year old about loops, how would you explain it?
 
+Have you ever had a lot of toys to put away, and you didn't want to put them away one by one? Loops let you do that on the computer all at once. You can make a computer check if what's on the floor is a toy, and put it away for you, then check the next thing on the floor and do the same! That way you can put them all away at once. When you have a lot of the same thing to do, and you don't want to do it one by one, you can use a loop to do it all at once! ~~If only I could do this with folding laundry too...~~
+
 6. What questions do you still have about Arrays and loops?
+
+I'm not sure I have any right now, I'm just excited to learn more about different array methods and exploring the possibilities of using them!
