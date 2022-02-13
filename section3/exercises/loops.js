29f2b89adb6fcc7ab2df2d7a90ef6fa284a48a26
Hannah Parker <hparke1402@gmail.com>
2022-02-12 23:51:54 -0500

    Correcting syntax and added code for last exercise

--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -23,22 +23,41 @@ for (var i = 0; i < 3; i++) {
 }
 
 // What is logged to the console for this second example? Why?
-// EXPLAIN:
+// EXPLAIN: The code is asking us to log the iteration of the loop variable `i`,
+//which stands for "index", three times. With this `i` variable, we establish the position in the
+// index the loop will start it's iterations. Initializing the loop to start at the first position,
+// which in JavaScript, is 0 since it is a zero-based language, the loop will run
+// three times total, logging the index value/position of the `i` variables each time and return
+// 0, 1, 2.
 
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
-
+for (var i = 0; i < 7; i++) {
+  console.log("4");
+}
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
+for (var i = 0; i < 10; i++) {
+  console.log("She sells seashells down by the seashore")
+}
 
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
+
+
+var text = "";
+var i;
+for (i = 0; i < 15; i++) {
+    text += "This is loop number:[" + i + "] ";
+    if (i!=0 && i%15==0);
+}
+console.log(text);
 
 // YOU DO: Using a for loop, how could you get an output that looks like this:
 
@@ -52,7 +71,11 @@ for (var i = 0; i < 3; i++) {
 // 3
 // 2
 // 1
+for (var i = 10; i >= 1; i-- > 0 )
+{
+    console.log(i);
+}
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your Mod 0 group on Slack!
