235c369603b2f17cf5544b7a52c7175c9117c562
Hannah Parker <hparke1402@gmail.com>
2022-02-13 08:03:04 -0500

    Completing final exercise

--- section3/exercises/loops-arrays.js
+++ section3/exercises/loops-arrays.js
@@ -18,21 +18,32 @@ for (var i = 0; i < animals.length; i++) {
 
 // YOU DO: Write code below that iterates through a list of animals and prints "The
 // <animal> is awesome!" for each animal.
-
+var animal = ["Cat", "Chicken", "Goldfish"];
+for (var i = 0; i < animal.length; i++) {
+  console.log("The " + animal[i] + " is Awesome!");
+}
 
 
 // YOU DO: Create an array of foods and then iterate over that array to log "Add
 // <food> to shopping list" for each food item.
-
+var foods = ["Watermelon", "Oranges", "Dragonfruit"];
+for (var i = 0; i < foods.length; i++) {
+  console.log("Add " + foods[i] + " to shopping list");
+}
 
 
 // YOU DO: Create an array of numbers and then iterate over that array to log double
 // of each of the number. (Ex: 4 doubled is 8.)
+var numbers = [1, 2, 4, 6, 8];
+for (var i = 0; i < numbers.length; i++ )
+console.log(numbers[i]*2);
 
-
-// YOU DO: Using the array of names below, print out "Happy Birthday <name>" for
-// each name except the first two.
+//YOU DO: Using the array of names below, print out "Happy Birthday <name>" for
+//each name except the first two.
 var names = ["Sevy", "Lindsey", "Jericho", "Raeonna", "Latrina", "Jhana", "Luca"];
+for (var i = 2; i < names.length; i++) {
+   console.log("Happy Birthday " + names[i] + "");
+}
 
 
 
@@ -42,8 +53,13 @@ var names = ["Sevy", "Lindsey", "Jericho", "Raeonna", "Latrina", "Jhana", "Luca"
   // 18 or over? "Make sure you get out and vote!"
 
 // WORD FROM THE WISE: Before you start coding, jot down, with pen and paper, what steps you
-// want to tell the program to take. Think of all the concepts you've learned so far. 
+// want to tell the program to take. Think of all the concepts you've learned so far.
 
 // Remember, stuck for 15-20 minutes? Reach out to your Mod 0 cohort on Slack.
-
 var ages = [17, 20, 34, 18, 16, 65, 49, 10, 22, 18];
+for (var i = 0; i < ages.length; i++)
+if (ages[i] <= 17) {
+  console.log("Oh no you can't quite vote yet.");
+} else if (ages[i] >= 18) {
+  console.log("Make sure you get out and vote!");
+}
