eb9d7397cf344ac8d00054a1dbbfa27b82905fa5
Cami Anderson <camidantas@icloud.com>
2022-02-23 21:17:14 -0500

    Add reflections and Decision making sec 2'

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -7,7 +7,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 2;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -39,15 +39,41 @@ Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
 
+In line 14 - 18 we have a if statement that executes a block of code if the
+condition doorChoice === 1 is true. If the condition is false, there is another
+blocks of code that can be executed.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+bearClothing.
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+bearClothing value will be scarf because of the condition in line 16 and 17.
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+
+In line 27 - 35 we have another if statement that executes a specific block of
+code if one of the conditions is true (bearChoice === 1, bearChoice === 2 or
+bearChoice === 3).
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+The final outcome will be: "You run as fast as you can into the next room. It's
+full of snakes!"
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+The final outcome will be:
+You enter a dark room with two doors. Do you go through #1 or #2?
+You see a bear putting on a hat
+It looks like that hat is too small for the bear, do you...
+1. Offer your own to the bear?
+2. Point it out to the bear?
+3. Make a dash for the next room?
+You tell the bear the hat is too small and it starts to cry!
+
 7. What is your favorite ending?
+
+My favorite ending is when the variable bearChoice === 1. :)
 */
