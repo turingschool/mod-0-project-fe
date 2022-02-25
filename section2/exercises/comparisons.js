69413b4c1808d187cb170dc16bc9ad93b58c91b4
Megan Schuetz <mschuetz411@gmail.com>
2022-02-25 11:20:32 -0700

    Add Section2

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -24,10 +24,10 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
-
+console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
 this statement still evaluated to true. JavaScript forces the stringTeachers into an integer to
@@ -36,7 +36,7 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
-
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
 since the numberTeachers is an integer value and the stringTeachers is a string value, although they are both equal to 4,
@@ -49,16 +49,16 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers != numberStudents);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
 
@@ -74,21 +74,21 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
-
+// This is going to check if 4 is less than 9 and return a boolean value of true or false, in this case true.
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
-
+// Here we have a variable defined as books and is assigned the number 3. The log is going to test if 4 is less than the variable books and return true or false, in this case false.
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
-
+// Two variables are defined: friends, assigned the number 6, and siblings, assigned the number 2. The log is going to check is friends greater than siblings and will return true.
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+//Two variables are defined: attendees, assigned the number 9, and meals, assigned the number 8. The log is going to test is attendees and meals strictly not equal to eachother and will return false.
 
 //-------------------
 // PART 3: Logical Operators
@@ -99,9 +99,9 @@ var finishedHomework = false;
 
 // EXAMPLE:
 // Determine if the user is hungry and has completed their homework
-console.log(isHungry && finishedHomework);
+console.log(isHungry && finishedHomework);//false
 // Determine if the user is hungry or has completed their homework
-console.log(isHungry || finishedHomework);
+console.log(isHungry || finishedHomework);//true
 
 var lovesToPlay = true;
 var lovesDogPark = false;
@@ -110,19 +110,20 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);//true
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);//false
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);//true
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age);//1
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
 // ANSWER:
-
+//The last line of code evaluates to 1.The && operator checks the first value if its false, which its not here, thus it returns the second value of 1.
 //-------------------
 // FINAL CHECK
 //-------------------
--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -38,16 +38,18 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
-
+This if/else statement is going to evaluate the variable doorChoice. Above it is already assigned number 1.
+Line 14 will check is doorChoice strictly equal to 1, which is true so the following code runs, reassigning the variable bearClothing to hat.
 2. What variable has a new value assigned to it after the first if statement executes?
-
+bearClothing gets reassigned after the if/else statement.
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
-
+The bearClothing value would be 'scarf'.
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
-
+The second if/else if/else statment is evaluating the bearChoice variable, originaly assigned to number 1. The first if statment is true and runs the following string.
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
-
+If bearChoice equalled 3 the outcome would be the second else if statement 'You run as fast as you can into the next room. It's full of snakes!'
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
-
+'You tell the bear the scarf is too small and it starts to cry!'
 7. What is your favorite ending?
+If your bearChoice was not 1,2 or 3 the outcome 'You stay with the bear and become it's best friend!' is cute and funny!
 */
