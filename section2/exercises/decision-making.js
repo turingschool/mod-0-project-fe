f6e2a2fd466cbe68ab59f839c56ae81e533e982a
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-02-26 13:25:02 -0500

    Add decision-making file

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -7,7 +7,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 2;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -39,15 +39,42 @@ Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
 
+If the doorChoice is equal to one, the condition will evaluate to true and the bearClothing
+will be assigned to hat. If the doorChoice is not equal to one, then the condition is false and the
+bearClothing choice will be assigned to scarf.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+The bearClothing variable has a new value to it after the first statement executes.
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+If I changed the variable doorChoice to equal 3, the condition would be false (3 is not equal to 1),
+so the else code block would execute and the bearClothing value would be "scarf".
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+
+When the variable bearChoice is equal to 1, then the condition evaluates to true and the line printed
+will be "You offer the bear your "bearClothing"and the bear shows you a secret passage out.
+If bearChoice is not equal to one, the condition resolves to false and it will check the next
+condition, and if it's equal to 2, then the line printed will be "You tell the bear the "bearClothing"
+is too small and it starts to cry!" If not equal to one or two, then next condition is checked which is the
+bearChoice equal to three, which if true will print out "You run as fast as you can into the next room. It's
+full of snakes!" If bearChoice is 4 or greater, the else code block will be executed and the line "You stay
+with the bear and become it's best friend!" is printed.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+If I changed the variable bearChoice to equal 3, then the 2nd else if coditional statement will run true and
+the line printed would be "You run as fast as you can into the next room. It's full of snakes!"
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+If I changed the variable doorChoice to equal 1, then the bearClothing will be 'hat.' And if the variable bearChoice
+is equal to 2, then the 1st else if condiitonal statement will run true and the line printed will be
+"You tell the bear the hat is too small and it starts to cry!"
+
 7. What is your favorite ending?
+
+My favorite ending is "You stay with the bear and become it's best friend!" :)
 */
