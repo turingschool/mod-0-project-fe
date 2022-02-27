c065ab15e2b114f96e6eb47512611798e84ee0e5
Lauren Black <laurenblack5280@gmail.com>
2022-02-26 18:17:22 -0700

    Add Section 1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,8 +22,8 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
-
+// Your answer and explanation here:
+//line 22 will be coerced into becoming a string by JavaScript
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,8 +31,8 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
-
-// YOU DO:
+console.log("The" + speedy + "jumped over the" + slowPoke);
+// YOU DO
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
@@ -40,16 +40,22 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
-
+console.log("In a predictable result, the" + slowPoke + "beat the" + speedy + "!");
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
+
+var eat = "see";
+var pray = "hear";
+var love = "speak";
+
+console.log(eat + "no evil" + pray + "no evil"+ love + "no evil");
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
--- section1/reflection.md
+++ section1/reflection.md
@@ -1,19 +1,25 @@
 ## Section 1 Reflection
 
 1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
-
+I tend to wait too long. I really like figuring things out on my own.
 2. What are the data types you learned about in this section? In your own words, define each.
-
+NUMBERS- used for numerical values
+STRINGS- any text wrapped in quotes
+BOOLEANS- used when the value can vary
 3. How would you log the string `"Hello World!"` to the console?
-
+```console.log("Hello World!")
+```
 4. What is/are the character(s) you would use to indicate comments in a JavaScript file? What is the purpose of a code comment?
-
+Two backslashes `//` indicate a comment. Code comments allow developers to remember why decisions were made
 5. In your own words, what is a variable? How would you explain it to a 5 year old?
-
+A variable is a way of storing a value for use in multiple places.
 6. Think of a site or app you use frequently. What are three variables that are probably used? Which data type would each of those variables probably hold?
+The variables would probably include Name, Birthdate, Online Status. They would employ these data types, respectively strings, numbers, and booleans.
 
 7. In your own words, explain what concatenation is.
-
+concatenation is joining together a string with one or more data types.
 8. Think of a site or app you use frequently. Where do you think the developers used concatenation?
-
+I believe zoom uses it. I accidently entered my age incorrectly and was kicked off. My guess is that the string included both
+my name and my birthdate which would be string in addition to a boolean.
 9. What questions do you still have about the work we've done so far? (not having a question is not an option)
+I'm not sure my annotation is correct when working with Booleans.
