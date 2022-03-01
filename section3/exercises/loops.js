c92966d10747cf0951b30859b448636233a6de01
Kristy Nguyen <knyen008@gmail.com>
2022-02-28 17:08:01 -0700

    Add loops.js

--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -23,25 +23,34 @@ for (var i = 0; i < 3; i++) {
 }
 
 // What is logged to the console for this second example? Why?
-// EXPLAIN:
-
+// EXPLAIN: What number loop we are on is the being logged to console beginning with the first loop
+// being 0. This is because our index = 0, and the console will continue to log what index we are on
+// until the loop has been repeated 3 times.
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
-
+for (var i = 0; i < 7; i++) {
+  console.log(2+2);
+}
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
-
+for (var i = 0; i < 10; i++) {
+  console.log('She sells seashells down by the seashore');
+}
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
-
+for (var i = 0; i < 3; i++) {
+  console.log("This is loop number " + i);
+}
 // YOU DO: Using a for loop, how could you get an output that looks like this:
-
+for (var i = 10; i >= 1; i--) {
+  console.log(i);
+}
 // 10
 // 9
 // 8
@@ -53,6 +62,6 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your Mod 0 group on Slack!
