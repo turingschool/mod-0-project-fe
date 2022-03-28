af9d147645f344c6c3edc21c14bc65770d3d1379
Stephanie Guzman <sguzman247@gmail.com>
2022-03-27 21:34:34 -0600

    Add Section 2 work

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -24,9 +24,11 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
+console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
@@ -36,7 +38,7 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
-
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
 since the numberTeachers is an integer value and the stringTeachers is a string value, although they are both equal to 4,
@@ -49,19 +51,23 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
+console.log("is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
-
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 //-------------------
 // PART 2: Articulating what you are doing
@@ -74,21 +80,39 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
+//The code is evaluating whether 4 is less than 9. In this line of code, 4 and 9 are both treated as number data types.
+//The presence of < also indicates we are looking at a comparison operation.
+//Therefore when the console log is run, it will run whether the number 4 IS less than the number 9.
+//This operation will return a TRUE boolean result because 4 is less than 9.
 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+//The variable books is given a number data type and value of 3.
+//This is also another comparison operation with the presence of <
+//The console log is comparing whether the number 4 is less than(<) the variable books(3).
+//With this information, we can conclude that the console log will determine whether "4 < 3".
+//When run, it will return a FALSE boolean result because the number 4 is not less than the number 3.
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+//The variables friends and siblings are both assigned number data types with values of 6(friends) and 2(siblings)
+//The presence of > also tells us this is a comparison operation
+//The console log is comparing whether friends(6) is greater than (>) siblings(2)
+//With this information, we can conclude that the console log will determine whether 6 > 2
+//When run, it will return a TRUE boolean result because the number 6 is greather than the number 2.
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+//The variables attendees and meals are both assigned number data tpes with values of 9(attendees) and 8(meals)
+//The presence of !== tells us that this is a comparison operation
+//The console log is then determining whether attendees(9) is not equal(!==) to meals(8)
+//With this information we can conclude that the console log is actually running whether 9 !== 8
+//When run, it witll return a TRUE boolean result because 9 does not equal 8.
 
 //-------------------
 // PART 3: Logical Operators
@@ -110,22 +134,29 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log("The dog loves to play and loves treats", lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log("The dog loves to play and loves the dog park", lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log("The dog loves to play or loves the dog park", lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+console.log("The dog loves to play and is a puppy", lovesToPlay && age <= 1);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
 // ANSWER:
+//TRUE
+//As written, we have not defined key pieces of information for the computer to run the operation as intended.
+//Specifically, we have not defined what a puppy is or how old a puppy is.
+//Therefore, the computer also does not know this information and cannot run the operation.
+//For the sake of this example, I used the age variable we already defined and then defined a puppy as 'age <= 1' in order to run the operation.
+//I chose to move forward with this example in order to produce a viable result.
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
 // Did you run this file in your terminal to make sure everything printed out to the console
-// as you would expect?
+// as you would expect? Yes
--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -38,16 +38,29 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
+The user is presented with two options on line 12: door 1 or 2. Line 14 begins the if conditional statement.
+If the user chooses door 1, the if statement on line 14 executes as true and the bearClothing variable for hat executes.
+If the user does not choose door 1, if statement on line 14 executes as false.
+The computer then runs the else statement on line 16 which executes as true and the bearClothing variable for scarf executes
 
 2. What variable has a new value assigned to it after the first if statement executes?
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+Changing the variable value doorChoice on line 8 results in the bearClothing value executing as scarf.
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+If the user selects bearChoice 1, the if statement on line 28 executes as true and is displayed to the user.
+If the user selects bearChoice 2, the else if statement on line 30 executes as true and is displayed to the user.
+If the user selects bearChoice 3, the else is statement on line 32 executies as true and is displayed to the user.
+If the user does not select bearChoice 1, 2, or 3, the else statement on line 34 executes as true and is displayed to the user.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+You stay with the bear and become its best friend!
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+You tell the bear the hat is too small and it starts to cry!
 
 7. What is your favorite ending?
+You offer the bear your hat and the bear shows you a secret passage out!
 */
