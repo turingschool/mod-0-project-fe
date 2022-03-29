ecc25cbffe649d91aaae09d58a84ae35e5f3f214
David Stavis <davidstavis@gmail.com>
2022-03-28 20:04:59 -0600

    Complete interpolation exercise

--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -22,8 +22,9 @@ var creatures = "unicorns";
 console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
 
+// It will log a string. Javascript uses type coersion to turn the number into a string so that it can be injected into the larger string, which is then printed to the console.
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,6 +32,7 @@ console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
+console.log(`The ${speedy} jumped over the ${slowPoke}`)
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -40,16 +42,25 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
+console.log(`In a predictable result, the ${slowPoke} beat the ${speedy}!`)
+
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation. 
+  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
+
+var number = 2
+var thing = "SHIELDS"
+var groupName = "SHIELD GANG"
+
+var statement = `Raise your ${number} ${thing}, and welcome to the ${groupName}!`
+console.log(statement)
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
