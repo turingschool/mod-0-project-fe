d288bba436130dc58331e78650d42524eb375bbe
Thomas Fettig <thomasmfettig@gmail.com>
2022-03-30 16:09:05 -0600

    Add Section 3 work

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,52 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length)
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = "Gorilla"
+console.log(animals)
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[3] = "Lion"
+console.log(animals)
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals[4] = "Elephant"
+console.log(animals[4])
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ["Pizza", "Hamburger", "Mac and Cheese", "Fried Chicken"]
+console.log(foods)
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length)
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push("Broccoli")
+console.log(foods)
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop()
+console.log(foods)
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.unshift("Fettuccine Alfredo", "Lasagna", "Meatballs")
+console.log(foods)
 
 // YOU DO: Remove the food that is in index position 0.
+foods.shift()
+console.log(foods)
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +89,16 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Facebook keeps a list of all of your friends
+var friends = ["Sara B", "Matt D", "Kevin S", "Adam S", "Logan K"]
+// 2: Schedulefly is the website my work uses to keep track of the weekly schedule. It keeps
+//    a list of my shifts from week to week.
+var schedule = ["Monday 10am", "Tuesday 11am", "Thursday 10am", "Friday 4pm"]
+// 3: Schedulefly also keeps a list of all current employees and their contact info.
+var employees = ["Eric R", "Zach K", "Alex F"]
 
 
 //-------------------
@@ -96,7 +110,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -107,6 +121,16 @@ var chargerType = "car";
 
 // Write your conditional here
 
+// This line will check if the battery is greater than or equal to 15
+if (percentBatteryLeft >= 15) {
+// If the battery is greather than or equal to 15, "Call an Uber" will print
+  console.log("Call an Uber");
+// If the battery is less than 15, this will check if the user has a charger, and
+// if it is a car charger.
+} else if (chargerType === "car" && hasCharger === true) {
+// If the user has a charger, AND it is a car charger, this line will print.
+  console.log("Charge your phone and then call an Uber");
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,12 +2,24 @@
 
 1. In Section 3, we removed even more supports and pushed you to learn a bit more independently, read through technical documentation, and use your resources more. How did that feel?
 
+I'm still feeling very good about all of this information.  I didn't have to use many outside sources to figure out how to write these lines of code.  I did have to look back at the section 2 exercises to remember the exact syntax of the if statements, but I think that those will become more engrained in my memory after some more practice writing them.
+
 2. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
+
+On Facebook, the developers likely use an Array to keep track of your friends list.  If someone sends you a friend request, you can click the "Accept" button, which likely performs a `push` method to add that friend into your current list, or Array.  Facebook also displays a total number of friends, which could be obtained using `array.length`.
 
 3. In your own words, explain what an Array method is.
 
+An array method is a function that performs a certain edit to the array.  In this section I learned about 4 methods: `pop`, to remove the *last* element of the array; `push`, to add one or more element to the *end* of the array; `shift` to remove the *first* element of array and thereby shifting the index; and `unshift`, to add one or more elements to the *front* of the array, also causing a shift in the index.
+
 4. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
+
+The purpose of a loop is to run the same command for multiple iterations of a variable i.  We would use this in conjunction with an array if we wanted to set the number of loops to be executed to the exact number of elements in the array.  We do this by using `i < array.length`.  If we know how many times we want the loop to run, we can use `i < n` and do not need to use it with an array.
 
 5. If you had to teach a 5 year old about loops, how would you explain it?
 
+I would explain that a loop is almost like a roller coaster ride that takes your picture at the end of every ride. However, in this roller coaster, it starts with only 1 person riding.  The coaster goes through its loop, and takes the picture of the 1 person riding, and then on the next ride 2 people are allowed on.  On the third ride, 3 people are allowed on, and so on. The ride ends when no more people can fit, or when the conductor decides to set a limit on the number of people.
+
 6. What questions do you still have about Arrays and loops?
+
+I learned about the pop/push/shift/unshift methods, but it seems those all deal with elements of the array that are either at the front or the back of the array.  Are there other methods to add or remove elements to the middle of the array or to a certain position? I know we can assign an array position by using `array[n] = "New Element"`, but what if the `n`th position is already taken, is there a way to add it to the middle of the array and still shift the index and not overwrite the current element for the `n`th position?
