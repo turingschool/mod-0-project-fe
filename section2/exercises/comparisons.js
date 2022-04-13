4012e71b53f9d646e3d6189364c50fd947320c9b
jmedina <ndgns2@gmail.com>
2022-04-13 15:36:22 -0600

    Add Section 2 work

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -19,14 +19,16 @@ var stringTeachers = "4";
 var numberDogs = 0;
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+console.log("Is numberTeachers greater than numberStudents?", + numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+console.log("Is numberTeachers less than numberStudents?", + numberTeachers < numberStudents)
 
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
+console.log("Is numberTeachers equal to stringTeachers?", + numberTeachers == stringTeachers)
 
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
@@ -36,6 +38,7 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
+console.log("Is numberTeachers strictly equal to stringTeachers?", + numberTeachers === stringTeachers);
 
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
@@ -49,19 +52,19 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers not equal to numberStudents?",+ numberTeachers != numberStudents);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents greater than or equal to 20?", + numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+console.log("Is numberStudents greater than or equal to 21?", + numberStudents >= 21);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents less than or equal to 20?", + numberStudents <= 20);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
-
+console.log("Is numberStudents less than or equal to 21", + numberStudents <= 21);
 
 //-------------------
 // PART 2: Articulating what you are doing
@@ -74,22 +77,26 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
-
+//The code is asking the console if the number 9 is greater than the number 4. The outcome should be true.
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+//The code is asking if the value of the books variable is greater than the number 4.
+//The outcome should be false since there are only 3 books.
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+// The code is asking if the friends variable is greater than the siblings variable.
+//The code is comparing the value of each variable. The outcome should be true since there are 6 friends and 4 siblings.
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
-
+//The code is checking the two vairables to see if they are equal, even though they are not
+//the operator is providing a strict rule to report the opposite answer. In this case true.
 //-------------------
 // PART 3: Logical Operators
 //-------------------
@@ -110,22 +117,29 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log("lovesToPlay and lovesTreats", lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
+console.log("lovesToPlay and lovesDogPark", lovesToPlay && lovesDogPark);
 
 
 // Determine if the dog loves to play or loves the dog park
+console.log("lovesToPlay or lovesDogPark", lovesToPlay || lovesDogPark);
 
 
 // Determine if the dog loves to play and is a puppy
+console.log("lovesToPlay and age", lovesToPlay && age);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
 // ANSWER:
-
+//The outcome of the code evaluated to "1" I think this is because it is comparing two different
+//types of variables, and when doing so it turns the values into numbers. Since this is a boolean type,
+//true is always 1 and false is always 0.
 //-------------------
 // FINAL CHECK
 //-------------------
 
 // Did you run this file in your terminal to make sure everything printed out to the console
 // as you would expect?
+
+//Yes!
--- section2/exercises/if-statements.js
+++ section2/exercises/if-statements.js
@@ -12,7 +12,7 @@ Make sure to run the file with node in your command line.
 // EXAMPLE:Manipulate the variable 'weather' to see if you can print something other
   // than 'coat'.
 
-var weather = 'snowy';
+var weather = '';
 // var weather = 'icy';
 // var weather = 'rainy';
 // var weather = 'sunny';
@@ -31,19 +31,29 @@ if (weather === 'sunny') {
   console.log('good to go!');
 }
 
-/* 
+/*
 YOU DO:
-Using the dogAge variable defined below, 
-determine if a dog is a puppy (2 or younger), 
+Using the dogAge variable defined below,
+determine if a dog is a puppy (2 or younger),
 an adult, or elderly (10 or older).
 
 Log to the console the appropriate age range
 (puppy, adult, elderly).
 */
 
-var dogAge = 3;
+var dogAge = 11;
 // Write your conditional here
+// var dogAge <= 2 puppy
+// var dogAge <= 10 adult
+// else = elderly
 
+if (dogAge <= 2){
+  console.log ('puppy');
+} else if (dogAge <= 10 ){
+  console.log('adult');
+} else {
+  console.log('elderly');
+}
 
 /*
 YOU DO:
@@ -62,11 +72,17 @@ When numQuarters = 2, program should log "I have enough money for a gumball"
 When numQuarters = 3, program should log "I have enough money for a gumball"
 */
 
-var numQuarters = 0;
+var numQuarters = 1;
+// var numQuarters >= 2 enough
+// else = not enough
 
 // Write your conditional here
+if (numQuarters >= 2){
 console.log("I have enough money for a gumball");
+}
+else {
 console.log("I don't have enough money for a gumball");
+}
 
 
 /*
@@ -87,10 +103,19 @@ var hasSauce = true;
 
 // Write your conditional here
 
+if (cupsOfFlour >=2 && hasSauce){
+console.log("I can make pizza")
+}
+else {
+  console.log("I cannot make pizza");
+}
+
+
 
 /*
-For the last exercise, an ideal solution probably uses a logical operator. 
+For the last exercise, an ideal solution probably uses a logical operator.
 Did yours? Do you know what a logical operator in JavaScript is? Google it to answer
 for yourself!
+ //I used the "&&" operator to to test more than one condition.
 
 */
