625197dd378ec7f2615e299f26ed7567e212e41b
jmedina <ndgns2@gmail.com>
2022-04-16 21:17:44 -0600

    Initial commit

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,52 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length)
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+var animals = ["Zebra", "Giraffe", "Elephant"];
+animals.push('Gorilla')
+console.log(animals)
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
-
+animals = ["Zebra", "Giraffe", "Elephant"];
+animals[2] = ('Unicorn');
+console.log(animals)
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
-
+animals = ["Zebra", "Giraffe", "Elephant"];
+console.log(animals[2])
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ["Pizza", "Salad", "Pasta", "Cheesecake"];
+console.log(foods)
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log (foods.length)
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push ("Broccoli");
+console.log (foods)
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
 
+console.log(foods.pop());
+console.log(foods)
 
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+foods.push("Tacos", "Burritos", "Enchiladas");
+console.log(foods)
 // YOU DO: Remove the food that is in index position 0.
-
+foods.shift();
+console.log(foods)
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +88,14 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Reddit Home Page - Within Reddits "home" page is an array of subreddits the user has subscribed to.
+// 2: Bank app - Within a users bank app the spending section displays multiple arrays of transactions that month.
+//categorizing them into dining, bills, travel, etc.
+// 3: Twitch - Twitch uses multiple arrays to narrow down the users preferences these arrays can be
+//game types, streaming categories, following, and even language.
 
 
 //-------------------
@@ -96,17 +107,32 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
 */
-var percentBatteryLeft = 12;
+var percentBatteryLeft =1;
 var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
-
+//Block of code execute if percentBatteryLeft variable has a value of 15 or higher
+if (percentBatteryLeft >=15){
+//Expected output:
+console.log("Congratulations! You can go home!");
+}
+//Block of code executes if percentBatteryLeft is less than 15,
+//user hasCharger is true, and the chargerType is "car"
+else if (hasCharger && chargerType === "car"){
+//Expected output:
+console.log("You need to charge your phone!");
+}
+//Block of code executes if the first two conditions evaluate to false.
+else {
+  //Expected output:
+ console.log("🚨 🆘 🚨")
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,13 +1,27 @@
 ## Section 3 Reflection
 
 1. In Section 3, we removed even more supports and pushed you to learn a bit more independently, read through technical documentation, and use your resources more. How did that feel?
+- Utilizing my resources and support felt comforting, this was the first time I was actually stuck and spent more than 30 minutes on a problem. However after reaching out to fellow classmates and friends it was nice knowing there are people willing to help. The gratification of finding information on my own and working through problems with out looking at previous sessions were well worth the struggle.
 
 2. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
+- Bank applications will use an array to track your `availableBalance`, within that array are a list of values. These values are the numerical transactions that have been considered settled. Each time your `availableBalance` is updated from those transactions the index starts at the amount you have left in your account.
 
 3. In your own words, explain what an Array method is.
+- An array is a macro variable, within that macro variable are a number of micro values each having their own identifier.(index)
 
 4. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
+- The purpose of a loop is to track and test multiple conditions based on a set of rules the user has declared, not stoping until it has found a true value. The purpose of using a loop in conjunction with an array is to to test the condition with all of the values inside of the array while keeping the code organized. Using a loop without an array is best for testing individual values on a smaller scale.
 
 5. If you had to teach a 5 year old about loops, how would you explain it?
+- If you are playing a video game and are currently on level 5, but you want to beat the game which doesn't happen until level 10 you continue trying.
+This is the beginning of the loop, the condition is you won't quit until you beat the game at level 10, every time you repeat the level you gain a little more experience that helps you move up in levels. Eventually you will be successful and win the game. 🎉
 
 6. What questions do you still have about Arrays and loops?
+- If an array is constantly logging new information, and your loop is set to run through all of the variables, what would be the conditional log command to pull a random variable from the array?
+
+For example how would I be able to determine who is the randomized winner from this Syntax
+
+```javascript
+var entries = ["Jim", "Bob", "Mo", "Larry"]
+for (i = 0; i < entries.length; i++)
+console.log("Congratulations " + `entries[i]`` + " you won!")
