9a61bc7c9ba0c6049d566b74aaf66e8b3b01a221
Anna Spitz <aspitzemail@gmail.com>
2022-04-16 20:18:28 -0500

    Revise Section 2 Decision Making

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -14,7 +14,7 @@ console.log("You enter a dark room with two doors. Do you go through #1 or #2?")
 if (doorChoice === 1) {
   bearClothing = "hat";
 } else {
-  bearClothing = "shirt";
+  bearClothing = "scarf";
 }
 
 console.log("You see a bear putting on a " + bearClothing);
@@ -39,9 +39,7 @@ Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
 
-   Lines 14 through 18 are a if statement. First JavaScript looks at the the if object, if it is
-   true then JavaScript will look no futher and it will chose the value for the variable bearClothing located within it's
-   code block. If if is false then JavaScript will chose else, using the value for bearClothing within the else code block.
+   Lines 14 through 18 contain a conditonal statement. First JavaScript will check the if statement. The peramiters of the if statement are that the element doorChoice are exactly equal to 1. If that if statemt evaluates to true the conditon will be met and the code block within it will run. The code within the if statement's code block sets bearClothing's value to "hat". If the if statement returns false then the else statement will execute. The else statement's code block sets bearClothing to "scarf".
 
 2. What variable has a new value assigned to it after the first if statement executes?
 
@@ -49,13 +47,11 @@ Questions
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
-  The bearClothing value will be the one located within the else code block.
+  The value of bearClothing will be that contained in the else statement's code block, "scarf".
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
 
-  JavaScript first checks the if statement on line 27. If that is false, it will move to the first else if on line 29, if that is
-  false it will move to the next else if statement on line 31, if that is false JavaScript will chose the else statment. At any
-  point if a code block exicutes then JavaScript will not look at the other statements. The conditons where satisfied.
+Lines 27 - 35 contain a conditonal statement that has one if statement, two else if statements and an else statement. Each of these has a code block contaning a string. The if statement and the first else if statement's strings contain a concantenation. Each statemnt, besides else, has peramiters that check to see if bearChoice is exactly equal to a number. JavaScript will first run the if statement and if it is true it's code block will exicute. If it's false JavaScript will run the first else if statement. If it is true, it's code block will execute, if false JavaScript will check the next else if statement. If that is true, it's code block will run. If it is false the code block in the else statement will run.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
