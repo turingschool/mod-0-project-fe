2503a7682799e27e026d7d41165fdd9d05ca161c
Shane Way <shane.c.way2@gmail.com>
2022-04-19 17:40:27 -0600

    Refactor decision-making.js file

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -39,16 +39,17 @@ Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
 
-  a) Between lines 14-18 we have an if/else statement that will decide if the bear is wearing a hat OR a scarf
-     based on the number value assigned to the doorChoice variable. Given the strict equality on line 14 it
-     is asking "is the value on the right the exact same as the value on the left"? If not, print scarf rather than hat.
+  a) Between lines 14-18 we have an if/else statement that will decide if the bear is wearing a hat (doorChoice = 1
+     OR a scarf (doorChoice = 2). Given the strict equality on line 14 the code is asking
+     "is the value on the right the exact same as the value on the left"? If not, print scarf rather than hat.
      Any disagreement between line 8 & 14 will always push the code to the else statement and the console will print "scarf".
 
 2. What variable has a new value assigned to it after the first if statement executes?
 
-  a) bearClothing will have a new value assigned to it after the first if statement executes.
-     Depending on the truth or falsity of this statement, bearClothing will be a hat or a scarf.
-     It is currently undefined in line 9 ('').
+  a) bearClothing is currently undefined in line 9 (''). It will have a new value assigned to it after
+     the first if statement executes. Depending on the truthy or falsy value of this statement,
+     bearClothing will be either a hat (=1) or a scarf (all other inputs).
+
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
@@ -56,15 +57,18 @@ Questions
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
 
-  a) Between lines 27 and 35 we have an if/else if/else conditional structure. As bearChoice value varies
-     from n = 1 --> n = 4 the if/else if/else structure will execute one of its four strict equality conditional outputs to
-     the console. The console outputs are independent from doorChoice and bearClothing in the dynamic story narrative.
+  a) Between lines 27 and 35 we have an if -else if - else conditional structure. As bearChoice value varies
+     from n = 1 --> n = 4 the if - else if - else structure will execute one of its four strict equality
+     conditional outputs to the console. Any integer >3 will execute the else condition. The bearClothing variable
+     is independent from the bearChoice variable and if -else if - else conditional. It will be logged to
+     the console if bearChoice = 1 or 2, and not for any other value in the conditonal.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
   a) You run as fast as you can into the next room. It's full of snakes!
 
-6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2,
+   what will be the final outcome be?
 
   a) You tell the bear the hat is too small and it starts to cry!
 
