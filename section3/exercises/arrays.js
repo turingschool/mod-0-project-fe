748362e68c938291f80c8773b78d684179def159
Emily Miles <ms.emily.j.miles@gmail.com>
2022-04-20 20:06:18 -0400

    Add arrays

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,53 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals.pop();
+animals.push('Gorilla')
+console.log(animals);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push('Cheetah');
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.unshift('Elephant');
+console.log(animals);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var veggies = ['Brussels Sprout', 'Parsnip', 'Cauliflower', 'Yams'];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(veggies);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+veggies.unshift('Broccoli');
+console.log(veggies);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+veggies.pop();
+console.log(veggies);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+var moreVeggies = ['Garlic', 'Shallot', 'Kale'];
+var combinedVeggies = veggies.concat(moreVeggies);
+console.log(combinedVeggies);
 
 // YOU DO: Remove the food that is in index position 0.
+combinedVeggies.shift();
+console.log(combinedVeggies);
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,7 +90,7 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
 // 1:
@@ -96,7 +107,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
