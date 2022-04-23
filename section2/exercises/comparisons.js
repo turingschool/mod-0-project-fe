98c7c6f37fc67de2e9596a0864d1c129d9a1a465
Emma Russell <103080131+nairnairnair@users.noreply.github.com>
2022-04-22 22:50:36 -0600

    Update comparisons.js

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -14,8 +14,11 @@ at the directions from Section 1 if you need a refresher on how to do that.
 //-------------------
 
 var numberTeachers = 4;
+
 var numberStudents = 20;
+
 var stringTeachers = "4";
+
 var numberDogs = 0;
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
@@ -24,10 +27,14 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+
 console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
+
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
+
 console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
+
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
 this statement still evaluated to true. JavaScript forces the stringTeachers into an integer to
@@ -36,7 +43,9 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
+
 console.log("is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
+
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
 since the numberTeachers is an integer value and the stringTeachers is a string value, although they are both equal to 4,
@@ -49,22 +58,27 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
+
 console.log("is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
+
 console.log("is numberStudents greater than or equal to 20?", numberStudents >= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+
 console.log("is numberStudents greater than or equal to 21?", numberStudents >= 21);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
+
 console.log("is numberStudents less than or equal to 20?", numberStudents <= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
+
 console.log("is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 
@@ -78,26 +92,41 @@ console.log("is numberStudents less than or equal to 21?", numberStudents <= 21)
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
+
 //YOU DO: Explain.
+
 //Logging to the console "Is 4 less than 9?" results in the Boolean value "True"
+
 var books = 3;
+
 console.log(4 < books);
+
 // YOU DO: Explain.
+
 //The variable "books" is assigned the number 3. Then, we log "Is (the number)
 //4 less than (the string) books?" which would return the Boolean value "false"
 //since books has been assigned the number 3.
+
 var friends = 6;
+
 var siblings = 2;
+
 console.log(friends > siblings);
+
 // YOU DO: Explain.
+
 //The variable "friends" is assigned the number 6; siblings is assigned the number 2.
 //"Is friends greater than siblings?" is logged to the console, resulting in the
 //Boolean value "true"
 
 var attendees = 9;
+
 var meals = 8;
+
 console.log(attendees !== meals);
+
 // YOU DO: Explain.
+
 //The variable "attendees" is assigned the number 9; meals is assigned the number 8.
 //"attendees is not equal to meals" is logged to the console, resulting in the Boolean
 //value "true" since 8 !== 9
@@ -133,13 +162,19 @@ console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
 console.log(lovesToPlay && age);
+
 // What did your final line of code evaluate to? Why do you think that is? Explain.
+
 // ANSWER:
+
 //It evaluated to 1. Age is entered as a number instead of as a string (if the variable was `var isPuppy = true`
 //then it would have resulted in the Boolean "true") but since lovesToPlay is true and true is associated with 1
 //in the same way false is with 0, I assume `console.log(1 && 1)` would come out to 1 as well. Let's test that out.
+
 console.log(1 && 1)
+
 //Yup, it does.
+
 //-------------------
 // FINAL CHECK
 //-------------------
