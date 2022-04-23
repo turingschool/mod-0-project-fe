800ccdc1cc8061865afc1ee2c551ec9142137c46
Emma Russell <103080131+nairnairnair@users.noreply.github.com>
2022-04-22 22:37:41 -0600

    Update dataTypes.js

--- section1/exercises/dataTypes.js
+++ section1/exercises/dataTypes.js
@@ -15,16 +15,20 @@ file from your terminal with the command `node section1/exercises/dataTypes.js`
 //-------------------
 
 // EXAMPLE: Write code below to log the String "Alan Turing" in the console:
-console.log("Alan Turing");
+
+console.log('Alan Turing');
 
 // EXAMPLE: Write code below to log `Welcome to Turing!` in the console:
+
 console.log('Welcome to Turing!');
 
 // YOU DO: Write code below to log `99 bottles of pop on the wall...`:
+
 console.log('99 bottles of pop on the wall...')
 
 // YOU DO: Write code below to log one line from your favorite song or movie:
-console.log("How disgusting.")
+
+console.log('How disgusting.')
 
 
 //-------------------
@@ -32,18 +36,23 @@ console.log("How disgusting.")
 //-------------------
 
 // EXAMPLE: log the result of the sum of 2 and 2 to the console:
+
 console.log(2 + 2);
 
 // EXAMPLE: log the result of 7 subtracted from 83 to the console:
+
 console.log(83 - 7);
 
 // YOU DO: log the result of 6 multiplied by 53 to the console:
+
 console.log(6 * 53)
 
 // YOU DO: log the result of 20 divided by 4 to console:
+
 console.log(20 / 4)
 
 // YOU DO: log the result of the modulo of 10 into 54:
+
 console.log(54 % 10)
 
 
@@ -52,16 +61,23 @@ console.log(54 % 10)
 //-------------------
 
 // EXAMPLE: log to the console the result of 1 is equal to 2:
+
 console.log(1 === 2);
 
 // EXAMPLE: log to the console the result of 7 is greater than 2:
+
 console.log(7 > 2);
 
 // YOU DO: log to the console the result of "hello" is equal to "Hello":
+
 console.log("hello" === "Hello")
+
 // YOU DO: log to the console the result of 3 is not equal to 4:
+
 console.log(3 != 4)
+
 // YOU DO: log to the console the result of 4 is less than or equal to 5:
+
 console.log(4 <= 5)
 
 
