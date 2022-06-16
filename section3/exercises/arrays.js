a0a895125ab4746e23c5b17a3dc3cfb6825d0d8f
Angela Wirth <angiewirth92@gmail.com>
2022-06-15 20:26:06 -0400

    completed

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -20,47 +20,54 @@ console.log(animals);
 
 // EXAMPLE: Write code below that will log "Zebra" from the animals array
 // YOUR CODE HERE
+var animals = ["Zebra", "Giraffe", "Elephant"];
 console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+var animals = ["Zebra", "Giraffe", "Elephant"];
+console.log(animals.length)
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = "Gorilla"
+console.log(animals)
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push('Lion');
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+var animals = ["Zebra", "Giraffe", "Elephant"];
+console.log(animals[2])
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var greenVeg = ['peas', 'asparagus', 'celery', 'lettuce']
+console.log(greenVeg);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(greenVeg.length)
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
-
+greenVeg.unshift ('broccoli')
+console.log(greenVeg);
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
-
-
-// YOU DO: Write code to add 3 new foods to the array. 
+greenVeg.pop()
+console.log[greenVeg.pop()]
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+greenVeg.push('kale','brussels sprouts', 'green beans');
+console.log[greenVeg.push()]
 // YOU DO: Remove the food that is in index position 0.
-
+greenVeg.shift
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +86,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Phone app - contacts are an array
+// 2: Facebook - posts on profile are an array
+// 3: Bank app - transactions in selected account are an array
 
 
 //-------------------
@@ -96,7 +103,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,6 +113,23 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+//The line below checks if the condition percentBatteryLeft is greater than or equal to 15 is true, if it
+//is the 'can call an uber' will print.
+if (percentBatteryLeft >= 15){
+  console.log('can call an uber')
+//This line checks to see if the condition chargerType is 'car' and hasCharger is true, is true. If it
+//is then 'can call an uber' prints.
+}else if (chargerType = 'car' && hasCharger = true){
+  console.log('can call an uber')
+//This line checks to see if the chargerType is not 'car' or if the hasCharger is false, making the
+//condition true and printing 'no uber'
+}else if (chargerType !== 'car' || hasCharger = false) {
+  console.log('no uber')
+//The last line checks if the condition that the percentBatteryLeft is less than 15 is true, if it
+//is then 'no uber' will print. 
+}else if (percentBatteryLeft <15){
+  console.log('no uber')
+}
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -18,29 +18,41 @@ for (var i = 0; i < 9; i++) {
 }
 
 // EXAMPLE: Write code that logs to the console the value of `i`, 3 times:
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i <= 3; i++) {
   console.log(i);
 }
 
 // What is logged to the console for this second example? Why?
 // EXPLAIN:
-
+////0,1,2 because "i" is 0, then the loop adds 1 which is 1 and that is still less than
+//3, making the condition true so it loops again, adding 1 which is 2, still less than
+//3, loops again but the output is no longer less than three so the condition is false
+//therefore, the code block stops running and nothing else is printed.
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
 
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
-
+for (var i = 0; i < 11; i++) {
+  console.log("She sells seashells down by the seashore")
+}
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
+for (var i = 0; i < 10; i++){
+  console.log("This is loop number " + i)
+}
 
 // YOU DO: Using a for loop, how could you get an output that looks like this:
+for (var i = 10; i >= 0; i--){
+  console.log(i)
+}
+
 
 // 10
 // 9
@@ -53,6 +65,6 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your Mod 0 group on Slack!
